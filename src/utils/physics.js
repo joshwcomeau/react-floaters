e5ebf274bdcf5530a99606bb03bed727126c8103
Joshua Comeau <joshwcomeau@gmail.com>
2016-07-03 09:11:23 -0400

    Update physics to accept positive stiffness/damping

--- src/utils/physics.js
+++ src/utils/physics.js
@@ -8,6 +8,14 @@ export const calculateSpringPosition = ({
   target,
   frameDuration,
 }) => {
+  // The `stiffness` and `damping` provided are likely given as positive numbers,
+  // when really the formula calls for negative ones.
+
+  /* eslint-disable no-param-reassign */
+  if (stiffness > 0) { stiffness *= -1; }
+  if (damping > 0) { damping *= -1; }
+  /* eslint-enable */
+
   const spring = stiffness * (offset - target);
   const damper = damping * velocity;
   const acceleration = (spring + damper) / mass;
